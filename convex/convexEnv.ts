f2547b27238afc23599371033c9a9a9f85378a0b
XxorinXx <orinhazan@gmail.com>
2025-12-08 20:07:44 +0200

    use helius in development

--- convex/convexEnv.ts
+++ convex/convexEnv.ts
@@ -7,9 +7,9 @@ export const PRIVY_APP_SECRET = process.env.PRIVY_APP_SECRET as string;
 export const JUPITER_API_KEY = process.env.JUPITER_API_KEY as string;
 
 export const HELIUS_API_KEY = process.env.HELIUS_API_KEY as string;
-export const RPC_URL = process.env.RPC_URL as string;
+export const RPC_URL = process.env.RPC_URL! as string;
 export const HELIUS_RPC_URL = `https://mainnet.helius-rpc.com/?api-key=${HELIUS_API_KEY}`;
-export const connection = new Connection(RPC_URL);
+export const connection = new Connection(RPC_URL ?? HELIUS_RPC_URL);
 export const JITO_UUID = process.env.JITO_UUID as string;
 
 export const NOZOMI_API_KEY = process.env.NOZOMI_API_KEY as string;
